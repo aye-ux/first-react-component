cc4cc4a5c29f4f69cf997307254879fb02ac1bce
K. Rucs <rucaskaye@gmail.com>
2025-07-31 23:11:19 +0800

    Updated CSS styles in global.css

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,9 +1,9 @@
 function Skills (){
     return(
         <ul>
-            <li>Eating</li>
-            <li> Sleeping</li>
-            <li>Reading</li>
+            <li>Critical Thinking</li>
+            <li> Adaptability</li>
+            <li>Creativity</li>
         </ul>
     )
 }
